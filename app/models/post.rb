b97ec8195bde12892f94745d58ecbe1f43497701
NathanB09 <nathanbell09@gmail.com>
2018-12-16 15:22:21 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate :clickbait?
+
+  def clickbait?
+    clickbaits = ["Won't Believe", "Secret", "Top #{/[0-9]*/}", "Guess"]
+    if title.present? && clickbaits.none? {|word| title.include?(word)}
+      errors.add(:title, "is not clickbait")
+    end
+  end
 end
