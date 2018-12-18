ad855f615d24f9ef8133845342350613232bb9e3
atkinsonholly <atkinsonholly@gmail.com>
2018-12-18 16:52:49 +0000

    Tests passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbateyness
+
+  def clickbateyness
+    array = ["Won't Believe", "Secret", "Top", "Guess"]
+    array.each do |phrase|
+      if title.to_s.include? phrase
+        return
+      else
+        errors.add(:title, "Not clickbatey enough! Please amend title.")
+      end
+    end
+  end
+
 end
